38e2c64d553daabeb3fca139ebdba2812fd1a9ff
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-10 18:50:55 -0400

    Add basic routes for user login / oauth login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  # user registration, login and authentication routes:
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  
+
 end
