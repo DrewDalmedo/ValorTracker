c5552d887e3704e473add60588d606d01e3d4fef
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-10 19:02:48 -0400

    Add root route (try saying that 10 times fast)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # root webpage
+  root 'application#home'
 
 
   # user registration, login and authentication routes:
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   get 'login' => 'session#new'
   post 'login' => 'session#create'
-  
+
 
 end
