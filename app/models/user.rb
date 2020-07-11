626787b0d0ded9bde77e28d863f9fef116288e85
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-11 16:14:45 -0400

    Add persistence of oauth users to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-    has_secure_password
+    # allows users to create an account without a password (used for omniauth),
+    # create a custom validation later for local accounts!!
+    has_secure_password :validations => false
+    
     has_many :matches
     has_many :guides
     has_many :agents, through: :matches
