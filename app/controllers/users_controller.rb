4c878f15a2047a9e2c6bcee624d40c49e201f99b
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-11 16:22:34 -0400

    Create users controller for managing user accounts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+    # adding just in case I decide to implement this
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    # once again, adding just in case
+    def destroy
+    end
+    
+end
