13591523355b47c511a99b90df777931d4a3f1ac
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-11 08:39:55 -0400

    Add basic omniauth session saving

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
+    # used for developer auth strategy
+    skip_before_action :verify_authenticity_token, only: :create
 
     def new
     end
 
     def create
+        session[:name] = request.env['omniauth.auth']['info']['name']
+        session[:omniauth_data] = request.env['omniauth.auth']
+        redirect_to root_path
     end
 
     def destroy
