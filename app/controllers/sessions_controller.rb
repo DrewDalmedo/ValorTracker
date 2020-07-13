48fb4849615de752aaeec4ebd1db0a9b4c2e7ca1
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-13 15:20:51 -0400

    Add logic for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,27 @@ class SessionsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: :create
 
     def new
+        if session[:name]
+            redirect_to root_path
+        end
+        render 'login'
     end
 
     def create
-        byebug
-
-        @user = User.find_by(name: params[:name])
-        session[:name] = @user.name if @user
-
-        redirect_to root_path
+        login_info = params[:user]
+        @user = User.find_by(name: login_info[:name])
+        #byebug
+        if @user && @user.authenticate(login_info[:password])
+            session[:name] = @user.name
+            redirect_to root_path
+        else
+            redirect_to login_path
+        end
     end
 
     def destroy
+        session.delete :name
+        redirect_to root_path
     end
 
 end
