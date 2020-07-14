ce636d8783a1b29d14612a2a6cae0baae65decf4
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-13 20:26:18 -0400

    Add validations for usernames and emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
     # allows users to create an account without a password (used for omniauth),
     # create a custom validation later for local accounts!!
     has_secure_password :validations => false
-    
+    # switch to regular has_secure_password once omniauth stuff is sorted out
+    #has_secure_password
+
+
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
     has_many :matches
     has_many :guides
     has_many :agents, through: :matches
