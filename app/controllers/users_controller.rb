b0d546b125a9df76fa0ef00eb10bc1f5771b6b4f
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-13 20:22:41 -0400

    Add user registration logic to create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,14 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to root_path
+        else
+            flash[:alert] = "That username or email is already in use!"
+            redirect_to register_path
+        end
     end
 
     def show
@@ -20,6 +27,12 @@ class UsersController < ApplicationController
 
     # once again, adding just in case
     def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
     
 end
