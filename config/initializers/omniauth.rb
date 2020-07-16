91cdad51d0499f8d8f4db56f5a30d852621b4370
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-16 12:55:49 -0400

    Add google oauth2 strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    # developer authentication
+    # development test authentication
     provider :developer unless Rails.env.production?
 
     # add auth types below
-    '''
-    planned auth strategies:
-        - google
-        - discord
-        - twitch
-    '''
-    # provider :google,  ENV['GOOGLE_KEY'],  ENV['GOOGLE_SECRET']
-    # provider :discord, ENV['DISCORD_KEY'], ENV['DISCORD_SECRET']
-    # provider :twitch,  ENV['TWITCH_KEY'],  ENV['TWITCH_SECRET']
+    provider :google_oauth2,  ENV['GOOGLE_CLIENT_ID'],  ENV['GOOGLE_CLIENT_SECRET']
 
 end
