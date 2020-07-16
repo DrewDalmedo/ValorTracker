0c6ede9fb24f7310c2fa3a6a9034f1c7c24fac9b
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-16 12:50:22 -0400

    Add show, edit, and update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :require_login, except: [:new, :create]
 
     # adding just in case I decide to implement this
     def index
@@ -20,9 +21,20 @@ class UsersController < ApplicationController
     end
 
     def show
+        #byebug
+        @user = User.find(params[:id])
     end
 
     def edit
+        #byebug
+        @user = User.find(params[:id])
+    end
+
+    def update
+        byebug
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
     end
 
     # once again, adding just in case
@@ -33,6 +45,10 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:name, :email, :password)
+    end
+
+    def require_login()
+        return head(:forbidden) unless session.include? :user_id
     end
     
 end
