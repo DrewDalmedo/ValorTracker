50ffa1caf1125b9bb52912495a4ca0e0eb3be47d
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-17 16:05:19 -0400

    Add name validation

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,7 @@
 class Map < ApplicationRecord
+    # validations
+    validates :name, presence: true, uniqueness: true
+
     has_many :matches
     has_many :guides
     has_many :users, through: :guides
