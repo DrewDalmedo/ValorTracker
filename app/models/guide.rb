35c15237845d9655ee3e9ed91ae8ad3f1809812b
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-19 12:12:04 -0400

    Add validations

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,4 +1,12 @@
 class Guide < ApplicationRecord
+    # validations
+    # title must be present
+    validates :title, presence: true
+    # minimum guide body length
+    validates :body, length: {minimum: 50}
+    # should never not be present, but just in case:
+    validates :map_id, presence: true
+
     belongs_to :map
     belongs_to :user
 end
