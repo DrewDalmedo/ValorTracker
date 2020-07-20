9aba89c907a4a349e130801dd09bb2342c793c27
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-20 05:31:38 -0400

    Create seed data for chris

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Map.create(name: "Ascent")
+Map.create(name: "Bind")
+Map.create(name: "Haven")
+Map.create(name: "Split")
